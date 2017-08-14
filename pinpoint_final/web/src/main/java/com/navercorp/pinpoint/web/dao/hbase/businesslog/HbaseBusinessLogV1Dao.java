3a1bbdebd44590beaf163fb6a5f7408b98daa8a0
孔逸涛 <kongyt0823@163.com>
2017-08-14 12:26:31 +0800

    修改web搜索数据的rowkey

--- pinpoint_final/web/src/main/java/com/navercorp/pinpoint/web/dao/hbase/businesslog/HbaseBusinessLogV1Dao.java
+++ pinpoint_final/web/src/main/java/com/navercorp/pinpoint/web/dao/hbase/businesslog/HbaseBusinessLogV1Dao.java
@@ -50,8 +50,8 @@ public class HbaseBusinessLogV1Dao implements BusinessLogV1Dao {
         byte[] bAgentId = BytesUtils.toBytes(agentId);
         byte[] bStatType = new byte[]{BusinessLogType.BUSINESS_LOG_V1.getRawTypeCode()};
 
-        byte[] bTransactionId = BytesUtils.toBytes(transactionId);
-        byte[] rowKey = new byte[AGENT_NAME_MAX_LEN + bStatType.length + bTransactionId.length];
+        byte[] bTransactionId = BytesUtils.toBytes((transactionId + "#"));
+        byte[] rowKey = new byte[AGENT_NAME_MAX_LEN + bStatType.length + bTransactionId.length + 1];
         BytesUtils.writeBytes(rowKey, 0, bAgentId);
         BytesUtils.writeBytes(rowKey, AGENT_NAME_MAX_LEN, bStatType);
         BytesUtils.writeBytes(rowKey, AGENT_NAME_MAX_LEN + bStatType.length, bTransactionId);
